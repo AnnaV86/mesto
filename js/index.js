5fbcf9dc380e088c0e77e15e56df2449229fe994
Anna Vidutina <a.vidutina@yandex.ru>
2022-01-26 00:13:03 +0500

    подключение карточек через js

--- js/index.js
+++ js/index.js
@@ -7,6 +7,45 @@ let profileAboutMe = document.querySelector('.profile__about-me');
 let nameText = document.querySelector('#profName');
 let aboutMe = document.querySelector('#profAboutMe');
 
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+  },
+];
+
+const cardPlaсe = document.querySelector('#element-item').content;
+const elements = document.querySelector('.elements');
+
+for (let i = 0; i <= initialCards.length - 1; i++) {
+  const cardElement = cardPlaсe.querySelector('.element-item').cloneNode(true);
+  cardElement.querySelector('.element-item__photo').src = initialCards[i].link;
+  cardElement.querySelector('.element-item__title').textContent =
+    initialCards[i].name;
+  elements.append(cardElement);
+  console.log(elements);
+}
+
 function profilePopupOpen() {
   nameText.value = profileName.textContent;
   aboutMe.value = profileAboutMe.textContent;
